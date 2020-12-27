09cc55782466baf1dc227e3f3fd92407371433be
Vijay Pal <vijay.nsut@gmail.com>
2020-12-27 23:14:39 +0530

    Sidebar Styling updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,21 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import './Sidebar.css';
 
 function Sidebar() {
     return (
         <div className='sidebar'>
-            
+            <div className='sidebar__profile'>
+                <div className='profile__background'>
+
+                </div>
+                <div className='profile__image'>
+                    <Avatar className='large' />
+                </div>
+                <h3 className='profile__name'>Vijay PAL</h3>
+                <h4 className='profile__sem'>Sem-4</h4>
+
+            </div>
         </div>
     )
 }

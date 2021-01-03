d915217c2c101812921b027dab1ff4736f2372ab
Vijay Pal <vijay.nsut@gmail.com>
2021-01-03 20:31:13 +0530

    Society name changes statically

--- src/SocietySidebar.js
+++ src/SocietySidebar.js
@@ -6,14 +6,14 @@ import SocietySidebarOption from './SocietySidebarOption';
 function SocietySidebar() {
     return (
         <div className='society-sidebar'>
-            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
-            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
-            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
-            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
-            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
-            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
-            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
-            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='Ashwamedh' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='Crescendo' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='Junoon' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='Mirage' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='Alliance' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='Debsoc' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='Capella' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='Enactus' /><hr/>
         
         </div>
     )

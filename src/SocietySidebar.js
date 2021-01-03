bd57c72760e3efe8a5ba6a262ee0c3f841e1b964
Vijay Pal <vijay.nsut@gmail.com>
2021-01-03 20:21:25 +0530

    SocietySidebar comp structured

--- src/SocietySidebar.js
+++ src/SocietySidebar.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './SocietySidebar.css';
+import SocietySidebarOption from './SocietySidebarOption';
+
 
 function SocietySidebar() {
     return (
         <div className='society-sidebar'>
-            
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
+            <SocietySidebarOption url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj' title='ashwamedh' /><hr/>
+        
         </div>
     )
 }

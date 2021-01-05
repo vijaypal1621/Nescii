5b5562742a0a1e88568047ce5a4c52bc819a090e
Vijay Pal <vijay.nsut@gmail.com>
2021-01-05 19:27:48 +0530

    Widgets comp added

--- src/Society.js
+++ src/Society.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import SocietySidebar from './SocietySidebar';
+import MessageSender from './MessageSender';
+import Post from './Post';
+import Feed from './Feed';
 
 function Society() {
     return (
         <div className='society' style={{'display':'flex'}}>
             <SocietySidebar />
-            
+            {/* <MessageSender />
+            <Post profilePic='https://lh3.googleusercontent.com/a-/AOh14Gh95KiyNVSSbq7jC1c5nNE1XbCyP1yryz-OC8M7Xg=s96-c-rg-br100' 
+            message='This gonna be insane this season'
+            timestamp='This is a Test timestamp'
+            username='Vijay PAL'
+            image='https://ipl2020schedule.co.in/wp-content/uploads/2019/07/Vivo-IPL-2020-Schedule.jpg' /> */}
+            <Feed />
         </div>
     )
 }

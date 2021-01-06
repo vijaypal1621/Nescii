c0d0c5e5592690536d06dc55d1b03b253ca21efb
Vijay Pal <vijay.nsut@gmail.com>
2021-01-06 23:17:21 +0530

    Events section completed

--- src/Events.js
+++ src/Events.js
@@ -14,6 +14,14 @@ function Events() {
              title='Street Play' 
              timeline='24 march 2021' 
              place='Netaji Subhash University of technology' />
+             <Event url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj'
+             title='Street Play' 
+             timeline='24 march 2021' 
+             place='Netaji Subhash University of technology' />
+            <Event url='https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj'
+             title='Street Play' 
+             timeline='24 march 2021' 
+             place='Netaji Subhash University of technology' />
 
             
         </div>

5e787bd56357620f8201da9c73b5431a95a2f384
Vijay Pal <vijay.nsut@gmail.com>
2021-01-06 23:41:54 +0530

    WidgetAbout comp styled and positioned

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Widgets.css';
 import Events from './Events';
-
+import WidgetAbout from './WidgetAbout';
 function Widgets() {
     return (
         <div className='widgets'>
             <Events />
-            
+            <WidgetAbout />
         </div>
     )
 }

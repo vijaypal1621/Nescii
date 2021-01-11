1e5e357691d7bffb58ddfae235038439fe3508b4
Aaryan <aaryanrajsarda@gmail.com>
2021-01-11 17:38:49 +0530

    Updated Header

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import HomeIcon from "@material-ui/icons/Home";
-import {
-  Avatar,
-  BottomNavigation,
-  BottomNavigationAction,
-  IconButton,
-  Tooltip,
-} from "@material-ui/core";
+import { Avatar, IconButton, Tooltip } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import ForumIcon from "@material-ui/icons/Forum";
 import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
@@ -18,6 +12,11 @@ import AssignmentIcon from "@material-ui/icons/Assignment";
 import { NavLink } from "react-router-dom";
 
 function Header() {
+  const [current, setCurrent] = useState("home");
+  useEffect(() => {
+    const currentPage = document.getElementById(current);
+    currentPage.classList.add("header__option--active");
+  }, [current]);
   return (
     <div className="header">
       <div className="header__left">
@@ -28,29 +27,49 @@ function Header() {
         </div>
       </div>
       <div className="header__center">
-        <div className="header__option header__option--active">
+        <div className="header__option" id="home">
           <Tooltip title="Home">
-            <NavLink to="/home">
+            <NavLink
+              to="/home"
+              onClick={() => {
+                const currentPage = document.getElementById(current);
+                currentPage.classList.remove("header__option--active");
+                setCurrent("home");
+              }}
+            >
               <HomeIcon fontSize="large" />
             </NavLink>
           </Tooltip>
         </div>
-        <div className="header__option">
+        <div className="header__option" id="societies">
           <Tooltip title="Societies">
-            <NavLink to="/societies">
+            <NavLink
+              to="/societies"
+              onClick={() => {
+                const currentPage = document.getElementById(current);
+                currentPage.classList.remove("header__option--active");
+                setCurrent("societies");
+              }}
+            >
               <ApartmentIcon fontSize="large" />
             </NavLink>
           </Tooltip>
         </div>
-        <div className="header__option">
+        <div className="header__option" id="resources">
           <Tooltip title="Resources">
-            <NavLink to="/resources">
+            <NavLink
+              to="/resources"
+              onClick={() => {
+                const currentPage = document.getElementById(current);
+                currentPage.classList.remove("header__option--active");
+                setCurrent("resources");
+              }}
+            >
               <AssignmentIcon fontSize="large" />
             </NavLink>
           </Tooltip>
         </div>
       </div>
-
       <div className="header__right">
         <div className="header__info">
           <Avatar />
--- src/SocietySidebar.js
+++ src/SocietySidebar.js
@@ -1,56 +1,76 @@
+import { IconButton, Modal, FormControl } from "@material-ui/core";
 import React from "react";
 import "./SocietySidebar.css";
 import SocietySidebarOption from "./SocietySidebarOption";
+import AddIcon from "@material-ui/icons/Add";
 
 function SocietySidebar() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="society-sidebar">
-      <SocietySidebarOption
-        url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
-        title="Ashwamedh"
-      />
-      <hr />
-      <SocietySidebarOption
-        url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
-        title="Crescendo"
-      />
-      <hr />
-      <SocietySidebarOption
-        url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
-        title="Junoon"
-      />
-      <hr />
-      <SocietySidebarOption
-        url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
-        title="Mirage"
-      />
-      <hr />
-      <SocietySidebarOption
-        url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
-        title="Alliance"
-      />
-      <hr />
-      <SocietySidebarOption
-        url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
-        title="Debsoc"
-      />
-      <hr />
-      <SocietySidebarOption
-        url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
-        title="Capella"
-      />
-      <hr />
-      <SocietySidebarOption
-        url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
-        title="Enactus"
-      />
-      <hr />
-      <SocietySidebarOption
-        url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
-        title="Quiz Club"
-      />
-      <hr />
-    </div>
+    <>
+      <Modal open={open} onClose={handleClose}>
+        <div>
+          <form></form>
+        </div>
+      </Modal>
+      <div className="society-sidebar">
+        <IconButton component="span">
+          <AddIcon onClick={handleOpen} />
+        </IconButton>
+        <SocietySidebarOption
+          url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
+          title="Ashwamedh"
+        />
+        <hr />
+        <SocietySidebarOption
+          url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
+          title="Crescendo"
+        />
+        <hr />
+        <SocietySidebarOption
+          url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
+          title="Junoon"
+        />
+        <hr />
+        <SocietySidebarOption
+          url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
+          title="Mirage"
+        />
+        <hr />
+        <SocietySidebarOption
+          url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
+          title="Alliance"
+        />
+        <hr />
+        <SocietySidebarOption
+          url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
+          title="Debsoc"
+        />
+        <hr />
+        <SocietySidebarOption
+          url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
+          title="Capella"
+        />
+        <hr />
+        <SocietySidebarOption
+          url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
+          title="Enactus"
+        />
+        <hr />
+        <SocietySidebarOption
+          url="https://yt3.ggpht.com/ytc/AAUvwnh2tXWXz84kQWn1D0thfl6EAl5PeiBe0FwA2BQEKw=s176-c-k-c0x00ffffff-no-rj"
+          title="Quiz Club"
+        />
+        <hr />
+      </div>
+    </>
   );
 }
 

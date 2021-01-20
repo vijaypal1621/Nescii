842a791df66039be885d5023e6187baecf198190
Vijay Pal <vijay.nsut@gmail.com>
2021-01-21 00:35:22 +0530

    Timestamp added

--- src/SocietyPost.js
+++ src/SocietyPost.js
@@ -10,7 +10,7 @@ function SocietyPost({ profilePic, image, username, timestamp, message }) {
         <Avatar src={profilePic} className="post__avatar" />
         <div className="post__topInfo">
           <h3>{username}</h3>
-          <p>timestamp..</p>
+          <p>{new Date(timestamp?.toDate()).toUTCString()}</p>
         </div>
       </div>
       <div className="post__bottom">

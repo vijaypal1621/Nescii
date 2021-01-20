75cdab5665894ff74d2106a11e04c89e926a34ba
Vijay Pal <vijay.nsut@gmail.com>
2021-01-21 00:32:28 +0530

    SocietyPosts linked with db

--- src/Society.js
+++ src/Society.js
@@ -1,8 +1,5 @@
 import React from "react";
 import SocietySidebar from "./SocietySidebar";
-import Widgets from "./Widgets";
-import SocietyMessageSender from "./SocietyMessageSender";
-import SocietyPost from "./SocietyPost";
 import "./Society.css";
 import {BrowserRouter as Router, Switch,Route} from 'react-router-dom';
 import SocietyChat from './SocietyChat';
@@ -10,15 +7,12 @@ import SocietyChat from './SocietyChat';
 function Society() {
   return (
     <div className="society" style={{ display: "flex" }}>
-
-        
-                  {/* Sidebar */}
             <Router>                  
           <div style={{ flex: 0.1 }}>
             <SocietySidebar />
           </div>
             <Switch>
-              <Route exact path="/rooms/:roomId">
+              <Route exact path="/societies/:societyId">
                 <SocietyChat/>
               </Route>
               <Route path="/">
@@ -26,14 +20,6 @@ function Society() {
               </Route>
             </Switch>
           </Router>
-          {/* React-Router ->chat-screen*/}
-        
-
-
-
-      
-
-      
     </div>
   );
 }
--- src/SocietyChat.js
+++ src/SocietyChat.js
@@ -1,14 +1,59 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import SocietyMessageSender from './SocietyMessageSender';
 import SocietyPost from './SocietyPost';
 import Widgets from './Widgets';
+import { useParams} from 'react-router-dom';
+import db from './firebase';
+
 
 function SocietyChat() {
+
+  const {societyId}  = useParams();
+  const [societyDetails, setSocietyDetails] = useState(null);
+  const [posts, setPosts] = useState([]);
+
+//    1. changes URL with useHistory() Hook
+//    2. connects to databse using useEffect() and listens to the state changes using useState() hhok
+//    3. Uses URL by useParams(roomId) to fetch room details from the database!
+//    4. useHistory() and useParams() are from react-router-dom
+//    5. useEffect() and useState() are fom react;
+
+      useEffect(() => {
+        if(societyId){
+            db.collection("societies")
+              .doc(societyId)
+              .onSnapshot( (snapshot)=> 
+              setSocietyDetails(snapshot.data()) 
+              )
+              db.collection('societies').doc(societyId)
+              .collection('posts')
+              .orderBy('timestamp', 'asc')
+              .onSnapshot((snapshot) => setPosts( 
+                      snapshot.docs.map(doc => doc.data() )
+                  )
+              )
+        }
+        
+        
+      }, [societyId]);
+
+
     return (
         <>
             <div style={{ flex: 0.5 }} className="society__feed">
         <SocietyMessageSender />
-        <SocietyPost
+        {posts.map(({image,message,profilePic,timestamp,username})=>(
+                    <SocietyPost message={message}
+                             timestamp={timestamp}
+                             username={username}
+                             profilePic={profilePic}
+                             image={image} 
+                    />
+                ))}
+
+
+
+        {/* <SocietyPost
           profilePic="https://lh3.googleusercontent.com/a-/AOh14Gh95KiyNVSSbq7jC1c5nNE1XbCyP1yryz-OC8M7Xg=s96-c-rg-br100"
           message="This gonna be insane this season"
           timestamp="This is a Test timestamp"
@@ -21,7 +66,7 @@ function SocietyChat() {
           timestamp="This is a Test timestamp"
           username="nescii"
           image="http://en.wikipedia.org/wiki/Special:FilePath/Netaji_Subhas_University_of_Technology.svg"
-        />
+        /> */}
       </div>
 
       <div style={{ flex: 0.4 }}>

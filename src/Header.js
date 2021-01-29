18e812ec338fff2cc704196aff2396fbfad47e79
Vijay Pal <vijay.nsut@gmail.com>
2021-01-30 01:06:32 +0530

    Header design fixed

--- src/Header.js
+++ src/Header.js
@@ -75,7 +75,20 @@ function Header() {
             </NavLink>
           </Tooltip>
         </div>
-        <MenuIcon style={{ color: " #16a596" }} fontSize="large" />
+        <div className="header__option d-block d-md-none" id="resources">
+          <Tooltip title="Resources">
+            <NavLink
+              to="/resources"
+              onClick={() => {
+                const currentPage = document.getElementById(current);
+                currentPage.classList.remove("header__option--active");
+                setCurrent("resources");
+              }}
+            >
+              <MenuIcon style={{ color: " #16a596" }} fontSize="large" />
+            </NavLink>
+          </Tooltip>
+        </div>
         
       </div>
       <div className="header__right col-6 order-1 order-md-3 col-md-4">
@@ -93,100 +106,4 @@ function Header() {
 
 export default Header;
 
-// import React, { useState, useEffect } from "react";
-// import "./Header.css";
-// import SearchIcon from "@material-ui/icons/Search";
-// import HomeIcon from "@material-ui/icons/Home";
-// import { Avatar, IconButton, Tooltip } from "@material-ui/core";
-// import AddIcon from "@material-ui/icons/Add";
-// import ForumIcon from "@material-ui/icons/Forum";
-// import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
-// import ApartmentIcon from "@material-ui/icons/Apartment";
-// import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-// import AssignmentIcon from "@material-ui/icons/Assignment";
-// import HelpIcon from '@material-ui/icons/Help';
-// import {useStateValue} from './StateProvider';
-// import {NavLink} from 'react-router-dom';
-// import {
-//   Collapse,
-//   Navbar,
-//   NavbarToggler,
-//   NavbarBrand,
-//   Nav,
-//   NavItem,
-  
-//   UncontrolledDropdown,
-//   DropdownToggle,
-//   DropdownMenu,
-//   DropdownItem,
-//   NavbarText
-// } from 'reactstrap';
 
-// function Header() {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggle = () => setIsOpen(!isOpen);
-//   const [{user}] = useStateValue();
-
-//   return (
-//     <div className="header">
-//       <Navbar color="light" light expand="md">
-// <NavbarBrand href="/home"> 
-//     <div className="header__left">
-//             <h2>nescii</h2>
-//             <div className="header__input">
-//               <SearchIcon />
-//               <label style={{ display: "none" }} htmlFor="search">
-//                 Search
-//               </label>
-//               <input
-//                 placeholder="Search nescii..."
-//                 type="text"
-//                 id="search"
-//                 name="search"
-//               />
-//             </div>
-//           </div>
-// </NavbarBrand>
-// <NavbarToggler onClick={toggle} />
-// <Collapse isOpen={isOpen} navbar>
-//   <Nav className="mr-auto" navbar>
-//     <NavItem>
-//       <NavLink href="/home">
-//             <div className="header__option" id="home">
-//                 <Tooltip title="Home">
-//                     <HomeIcon style={{ color: " #16a596" }} fontSize="large" />
-//                 </Tooltip>
-//               </div>
-//       </NavLink>
-//     </NavItem>
-//     <NavItem>
-//       <NavLink href="/societies">
-//           <div className="header__option" id="societies">
-//               <Tooltip title="Societies">
-//                   <ApartmentIcon style={{ color: " #16a596" }} fontSize="large" />
-//               </Tooltip>
-//             </div>
-//       </NavLink>
-//     </NavItem>
-//     <NavItem>
-//       <NavLink href="/home">
-//           <div className="header__option" id="resources">
-//               <Tooltip title="Resources">
-//               <AssignmentIcon style={{ color: " #16a596" }} fontSize="large" />
-//               </Tooltip>
-//             </div>
-//       </NavLink>
-//     </NavItem>
-    
-//   </Nav>
-// </Collapse>
-// </Navbar>
-
-
-//     </div>      
-//   );  
-  
-// }
-
-// export default Header;

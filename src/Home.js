4218c2e360e939f1c606187e4bf7bec65a687dd5
Vijay Pal <vijay.nsut@gmail.com>
2021-01-29 16:53:29 +0530

    mobile view handled

--- src/Home.js
+++ src/Home.js
@@ -7,13 +7,13 @@ import Notices from "./Notices";
 function Home() {
     return (
         <div className='home row'>
-            <div className="col-lg-3 col-xl-2" style={{padding:"0"}}>
+            <div className="d-none d-md-block col-md-3 col-xl-2" style={{padding:"0"}}>
                 <Sidebar />
             </div>
-            <div className="col-lg-6 col-xl-6 " style={{padding:"0"}}>
+            <div className="col-12 col-sm-12 col-md-6 col-xl-6 " style={{padding:"0"}}>
                 <Feed />
             </div>
-            <div className="col-lg-3 col-xl-4" style={{padding:"0"}}>
+            <div className="d-none d-md-block col-md-3 col-xl-4" style={{padding:"0"}}>
                 <Notices />
             </div>
         </div>

fb2ac02014f160d2baa8156807eb18e84e8a26df
Vijay Pal <vijay.nsut@gmail.com>
2021-02-02 19:44:06 +0530

    Popover containg photo input enabled

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -24,7 +24,6 @@ function getModalStyle() {
   };
 }
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
@@ -40,7 +39,12 @@ const useStyles = makeStyles((theme) => ({
     width: theme.spacing(10),
     height: theme.spacing(10),
   },
+  typography: {
+    padding: theme.spacing(2),
+  },
 }));
+
+
 
 const useModalStyles = makeStyles((theme) => ({
   paper: {
@@ -55,16 +59,31 @@ const useModalStyles = makeStyles((theme) => ({
 
 function Sidebar() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
   const modalClasses = useModalStyles();
   const ref = useRef(null)
   const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = React.useState(false);
+  const [openModal, setOpenModal] = React.useState(false);
   const [{ user }] = useStateValue();
   const [profile,setProfile] = useState(null);
   const [photo,setPhoto] = useState(user?.photoURL);
   const [branch,setBranch]=useState('');
   const [year,setYear]=useState('');
   const [section,setSection]=useState('');
+
+
+  const handlePopClick = (event) => {
+    if(user?.email.includes('gmail')===false){
+      setAnchorEl(event.currentTarget);
+    }
+    
+    
+  };
+  const handlePopClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
   
         useEffect(() => {
           if(user?.email.includes('gmail')===false){
@@ -77,35 +96,17 @@ function Sidebar() {
           },
        [user?.email, user?.uid])
 
-  const openModal = () =>{
-    setOpen(true);
+  const handleOpenModal = () =>{
+    setOpenModal(true);
   }
   const handleClose = () => {
-    setOpen(false);
+    setOpenModal(false);
   };
 
-
-  const handlePhoto = () => {
-    if(user?.email.includes('gmail')===false){
-      <Popover
-        open
-        anchorEl={ref}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-      >
-        <Typography className={classes.typography}>The content of the Popover.</Typography>
-      </Popover>
-    }
-    else{
-        
-    }
+  const handlePhotoUpdate = () =>{
+    
   }
+
 
   const handleUpdate = (e) => {
     e.preventDefault();
@@ -120,7 +121,7 @@ function Sidebar() {
       setBranch('');
       setSection('');
       setYear('');
-      setOpen(false);
+      setOpenModal(false);
   };
   const body = (
     <div style={modalStyle} className={modalClasses.paper}>
@@ -137,10 +138,6 @@ function Sidebar() {
             Section
           </InputLabel>
         <input value={section} id="section" onChange={(e)=>setSection(e.target.value)} />
-        <InputLabel shrink htmlFor="postImage">
-            Photo
-        </InputLabel>
-        <input accept="image/*" id="postImage" type="file" onChange={(e)=>setPhoto(e.target.value)} />
         <Button style={{ color: "white", backgroundColor: "#16a596" }} onClick={handleUpdate}>Update</Button>
     </div>
   
@@ -155,12 +152,35 @@ function Sidebar() {
         <div className="sidebar__profile">
           <div className="profile__background"></div>
           <div className="profile__image">
+          <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handlePopClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Typography className={classes.typography}>
+        <InputLabel shrink htmlFor="postImage">
+            Photo
+        </InputLabel>
+        <input accept="image/*" id="postImage" type="file" onChange={(e)=>setPhoto(e.target.value)} />
+        <Button style={{ color: "white", backgroundColor: "#16a596" }} onClick={handlePhotoUpdate}>Update</Button>
+        </Typography>
+      </Popover>
             <Avatar
               alt={user?.displayName}
               src={user?.photoURL}
               ref={ref}
               className={classes.large}
-              onClick={handlePhoto}
+              aria-describedby={id} variant="contained" color="primary" onClick={handlePopClick}
+              // onClick={handlePhoto}
             />
           </div>
           <h3 className="profile__name">{user?.displayName}</h3>
@@ -169,23 +189,23 @@ function Sidebar() {
           ) : (
             <>
           <div className="row">
-              <h6 className="col-6 profile__info">Expected year of Graduation </h6>
+              <h6 className="col-5 text-left pl-2 profile__info">Expected year of Graduation </h6>
               <p  className="col-1 profile__info">:</p>
               <h6 className="col-5 profile__info">{profile?.year}</h6>
           </div>
           <div className="row">
-                <h6 className="col-6 profile__info">Branch</h6>
+                <h6 className="col-5 text-left pl-2 profile__info">Branch</h6>
                 <p  className="col-1 profile__info">:</p>
                 <h6 className="col-5 profile__info">{profile?.branch}</h6>
           </div>
           <div className="row">
-                <h6 className="col-6 profile__info">Section</h6>
+                <h6 className="col-5 text-left pl-2 profile__info">Section</h6>
                 <p  className="col-1 profile__info">:</p>
                 <h6 className="col-5 profile__info">{profile?.section}</h6>
           </div>
-          <EditIcon onClick={openModal} />
+          <EditIcon onClick={handleOpenModal} />
           <Modal
-          open={open}
+          open={openModal}
           onClose={handleClose}
           aria-labelledby="simple-modal-title"
           aria-describedby="simple-modal-description"
@@ -197,6 +217,9 @@ function Sidebar() {
 
           
           <Button onClick={() => auth.signOut()}>Logout</Button>
+          {/* <Button aria-describedby={id} variant="contained" color="primary" onClick={handlePopClick}>
+        Open Popover
+      </Button> */}
         </div>
       </div>
     </div>

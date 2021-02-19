f46bf192d4bc3d79f6a2374b5c7e6a423d2fcb18
Aaryan <aaryanrajsarda@gmail.com>
2021-02-19 19:05:57 +0530

    Society Post Redux Integration

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,12 +3,14 @@ import { Notices } from "./notices";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import { Posts } from "./posts";
+import { SocPosts } from "./socPosts";
 
 export const ConfigureStore = () => {
   const store = createStore(
     combineReducers({
       notices: Notices,
       posts: Posts,
+      socPosts: SocPosts,
     }),
     applyMiddleware(thunk, logger)
   );
